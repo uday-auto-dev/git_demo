6c40f2900876e700c0000b01b9fed4409e10b7c6
uday-auto-dev <udaydev368@gmail.com>
2025-05-30 11:36:33 +0530

    add mult_number method into operations.py module

--- hello/operations.py
+++ hello/operations.py
@@ -2,3 +2,8 @@
 def add_numbers(a: int, b:int):
     print("Adding numbers method is called !!!!")
     print(f"The addition of {a} + {b} is : ", a + b)
+
+
+def mult_number(a: int, b: int):
+    print("Multiplying numbers method is calling !!!")
+    print(f"The multiplication of {a} * {b} is : ", a * b)
