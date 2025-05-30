3c9671d0cae55c8ba975f971e912631cc55c3a9a
uday-auto-dev <udaydev368@gmail.com>
2025-05-30 08:47:44 +0530

    This is my first commit message

---
+++ hello/hello_user.py
@@ -0,0 +1,3 @@
+
+def hello_to_user(name):
+    print(f"Hello {name} Welcome to the World of Automation !!!!!!")
---
+++ main.py
@@ -0,0 +1,4 @@
+
+from hello.hello_user import hello_to_user
+
+hello_to_user("uday")
